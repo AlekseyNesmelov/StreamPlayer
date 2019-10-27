cbc3a639c3da1174c3727e3e2c5479f1a6d7a2d8
alexeynesmelov <Nesmelov.A.Ev@sberbank.ru>
2019-10-27 21:06:54 +0300

    clean up gradle files

--- lib-player/src/androidTest/java/ru/leksiinesm/playerlib/ExampleInstrumentedTest.java
+++ lib-player/src/androidTest/java/ru/leksiinesm/playerlib/ExampleInstrumentedTest.java
@@ -1,8 +1,10 @@
 package ru.leksiinesm.playerlib;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
